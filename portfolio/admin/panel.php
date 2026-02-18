5ffb1391b14256ca33aa714e795c7082ffdc8b53
Selam Bel <116879445+SelamBel@users.noreply.github.com>
2026-02-18 17:47:35 +0100

    Basic Menu for Admin Panel

--- portfolio/admin/panel.php
+++ portfolio/admin/panel.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $rol = $_SESSION["rol"] ?? null;
-if ($_SESSION["rol"] != "ADMIN") {
-    header("Location: index.php");
+if ($rol != "ADMIN") {
+    header("Location: ../index.php");
     exit();
 }
 ?>
@@ -17,6 +17,7 @@ if ($_SESSION["rol"] != "ADMIN") {
     <title>Panel Admin</title>
     <link rel="stylesheet" href="../static/css/topnav-footer.css">
     <link rel="stylesheet" href="../static/css/style.css">
+    <link rel="stylesheet" href="../static/css/private.css">
 </head>
 
 
@@ -24,7 +25,36 @@ if ($_SESSION["rol"] != "ADMIN") {
     <?php require_once '../templates/nav-admin.php'; ?>
 
     <main>
-        
+        <div class="panel">
+            <h1>Administración de proyectos (Admin)</h1>
+            <div class="panelBotones">
+                <div class="botonesFiltro">
+                    <div class="botonesFiltroIzq">
+                        <label for="cat">
+                            Filtrar por categoria:
+                            <select>
+                                <option>Front</option>
+                                <option>Back</option>
+                                <option>Full</option>
+                            </select>
+                        </label>
+                        <button>Filtrar</button>
+                    </div>
+                    <div class="botonesFiltroDer">
+                        <button id="exportCSV" name="exportCSV">Exportar CSV</button>
+                        <button id="newProyect" name="newProyect">Nuevo Proyecto</button>
+                    </div>
+                </div>
+            </div>
+            <table>
+                <th>Titulo</th>
+                <th>Descripcion</th>
+                <th>Categoria</th>
+                <th>Tecnologías</th>
+                <th>Acciones</th>
+            </table>
+        </div>
+
     </main>
 
     <?php require_once '../templates/footer-admin.php'; ?>
